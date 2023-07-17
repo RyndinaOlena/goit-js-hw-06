238113047fb63949a82e901a26675d916136d19f
Olena Ryndina <ryndinaolena@gmail.com>
2023-07-17 09:58:37 +0300

    10 task

--- js/task-10.js
+++ js/task-10.js
@@ -20,3 +20,31 @@ function getRandomHexColor() {
 //     }
 //     return amountEl
 // }
+
+const btnCreate = document.querySelector('[data-create]')
+const btDestroy = document.querySelector('[data-destroy]')
+const boxesEl = document.getElementById('boxes')
+const input = document.querySelector('input')
+
+btnCreate.addEventListener('click', createBoxes)
+
+function createBoxes(amount) {
+    let num = 30;
+    let array = []
+    amount = input.value
+    console.log(amount)
+    for (let i = 1; i <= amount; i += 1) {
+        const newDiv = document.createElement('div');
+        num += 10;
+        newDiv.style.width = `${num}px`;
+        newDiv.style.height = `${num}px`;
+        newDiv.style.backgroundColor = getRandomHexColor()
+        array.push(newDiv);
+
+    }
+    boxesEl.append(...array)
+}
+
+btDestroy.addEventListener('click', () => {
+    boxesEl.innerHTML = ""
+})
